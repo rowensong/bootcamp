26ec4170c40d946747420189440f469efbead9c3
Rowen <rowen.song@tosslab.com>
2023-04-10 20:22:14 +0900

    반복문 스터디

---
+++ class/03-javascript/반복문.js
@@ -0,0 +1,21 @@
+for (let index = 0; index < children.length; index++) {
+    console.log(children[index] + '입니다')
+}
+
+for (let index = 0; index < children.length; index++) {
+    console.log(children[index] + '입니다')
+}
+
+let persons = [
+    {name:'철수',age:17},{name:'ab',age:22},{name:'dnr',age:27}
+]
+
+for (let index = 0; index < persons.length; index++) {
+    console.log(persons[index].name)
+}
+
+for (let index = 0; index < persons.length; index++) {
+    console.log(persons[index].name)
+    if (persons[index].age >= 19) { console.log('adult') } else {console.log('kid')}
+}
+
