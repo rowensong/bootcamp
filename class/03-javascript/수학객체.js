5bf3df6498214909d7f162aed8ed595de86f3045
Rowen <rowen.song@tosslab.com>
2023-04-10 20:29:04 +0900

    수학객체 스터디

---
+++ class/03-javascript/수학객체.js
@@ -0,0 +1,20 @@
+String(Math.floor(Math.random() * 1000000)).padStart(6,"0")
+// '110310'
+String(Math.floor(Math.random() * 1000000)).padStart(6,"0")
+// '419578'
+String(Math.floor(Math.random() * 1000000)).padStart(6,"0")
+// '527696'
+String(Math.floor(Math.random() * 1000000)).padStart(6,"0")
+// '627960'
+String(Math.floor(Math.random() * 1000000)).padStart(6,"0")
+// '887275'
+String(Math.floor(Math.random() * 1000000)).padStart(6,"0")
+// '180389'
+String(Math.floor(Math.random() * 1000000)).padStart(6,"0")
+// '038757'
+String(Math.floor(Math.random() * 1000000)).padStart(6,"0")
+// '409572'
+let result = String(Math.floor(Math.random() * 1000000)).padStart(6,"0")
+// undefined
+// result
+// '872472'
